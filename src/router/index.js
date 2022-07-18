9dade1bdc006682934a2d6e424ee84739a016e69
zlx01 <761451281@qq.com>
2022-07-19 01:01:43 +0800

    vite批量引入组件

--- src/router/index.js
+++ src/router/index.js
@@ -1,24 +1,29 @@
 import { createRouter, createWebHashHistory } from "vue-router";
 import HomeView from "../views/HomeView.vue";
+const principleComponents = import.meta.globEager(
+  "../views/PrincipleView\\d{1,}.vue"
+);
 
-const routes = [
-  {
-    path: "/",
-    name: "home",
-    component: HomeView,
-  },
-];
-for (let i = 0; i < 12; i++) {
-  routes.push({
-    path: `/principle${i + 1}`,
-    name: `principle${i + 1}`,
-    component: () => import(`../views/PrincipleView${i + 1}.vue`),
-  });
-}
+const routes = Object.keys(principleComponents).map((key) => {
+  const principle = principleComponents[key].default;
+  const index = key.match(/\/PrincipleView(.*)\.vue$/)[1];
+  return {
+    path: "/principle" + index,
+    name: "Principle" + index,
+    component: principle,
+  };
+});
 
 const router = createRouter({
   history: createWebHashHistory(import.meta.env.BASE_URL),
-  routes,
+  routes: [
+    {
+      path: "/",
+      name: "home",
+      component: HomeView,
+    },
+    ...routes,
+  ],
 });
 
 export default router;
